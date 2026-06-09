1739e9cb546e2ae443a41b8b4e8b8a0687fde358
D3ssnk <65511755+D3ssnk@users.noreply.github.com>
2026-06-09 14:15:14 +0100

    created add tests and TaskManager Class

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-def add(x,y):
-    return x+3
+import os
+class TaskManager():
+    def __init__(self,json_path):
+        if not os.path.exists(json_path) or os.path.getsize(json_path) == 0:
+            with open(json_path, "w") as file:
+                file.write('{"task":[]}')
+        self.json_path = json_path
+
+    def add(self, task):
+        return task
+
+def main():
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
+        
--- test_main.py
+++ test_main.py
@@ -1,4 +1,32 @@
-from main import add
+from main import TaskManager
+import json
+import pytest
+import os
 
-def test_add():
-    assert add(1,2) == 3, "1 + 2 should be 3"
+@pytest.fixture
+def test_json_file():
+    if not os.path.exists("test_json.json"):
+        with open("test_json.json","w") as file:
+            file.write('{"tasks":[]}')
+    yield "test_json.json"
+    if os.path.exists("test_json.json"):
+        os.remove("test_json.json")
+    
+
+@pytest.fixture
+def test_task_manager(test_json_file):
+    return TaskManager(test_json_file)
+
+def test_add(test_task_manager, test_json_file):
+    tmp_tasks = {"tasks":[{'id': 1, 'description': 'Make Coffee'}]}
+    test_task_manager.add("Make Coffee")
+
+    with open(test_json_file, "r") as file:
+        file_data = json.load(file)
+
+    assert file_data == tmp_tasks, "Correctly adds task with ID as one"
+
+    test_task_manager.add("Make Tea")
+    tmp_tasks["tasks"].append({'id': 2, 'description': 'Make Tea'})
+    
+    assert file_data == tmp_tasks, "Correctly adds task with incremented ID"
